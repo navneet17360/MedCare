665405f6b8be35527fbadbff2eb94b0a48f8fba2
navneet17360 <105434354+navneet17360@users.noreply.github.com>
2025-05-04 11:07:54 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
 from config import Config
 from extensions import db  # <<< Import from extensions
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+      CORS(app) 
 
     db.init_app(app)  # <<< Important
 
